162e6848b1bfc8126a7da9c14bf0526f7db18e9f
Skull Vega <eslamonsy56@gmail.com>
2023-12-20 14:37:58 +0200

    initiallization

---
+++ js/index.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    var scrollorama = $.scrollorama({ blocks:'.scrollblock' });
+
+    // scrollorama.animate('#largeTriangle1>img', {
+    //     offset: 100,
+    //     duration: 500,
+    //     property: 'tansform',
+    //     easing: 'easeInOutQuint',
+    //     start: {translateY: '0%'},
+    //     end: {translateY: '-300%'}
+    // })
+    // scrollorama.animate('#mediumTriangle1>img', {
+    //     offset: 100,
+    //     duration: 500,
+    //     property: 'tansform',
+    //     easing: 'easeInOutQuint',
+    //     start: {translateY: '-100%'},
+    //     end: {translateY: '300%'}
+    // })
+    // scrollorama.animate('#smallTriangle1>img', {
+    //     offset: 100,
+    //     duration: 500,
+    //     property: 'tansform',
+    //     easing: 'easeInOutQuint',
+    //     start: {translateY: '0%'},
+    //     end: {translateY: '-5000%'}
+    // })
+    // scrollorama.animate('#smallTriangle2>img', {
+    //     offset: 100,
+    //     duration: 500,
+    //     property: 'tansform',
+    //     easing: 'easeInOutQuint',
+    //     start: {translateY: '0%'},
+    //     end: {translateY: '-100%'}
+    // })
+
+});
